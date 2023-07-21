76e4de4fc1e0833913393243788f3a2f976f434d
Rasul <rasul.zeynalov.95@gmail.com>
2023-07-21 15:15:57 +0400

    update

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+"use client";
 
-const CustomButton = () => {
+
+import Image from "next/image"
+import { CustomButtonProps } from "@/types";
+
+const CustomButton = ({title, containerStyles, handleClick} : CustomButtonProps) => {
   return (
-    <div>CustomButton</div>
+    <button
+    disabled={false}
+    type={"button"}
+    className={`custom-btn ${containerStyles}`}
+    onClick={() => {}}
+    >   
+    <span className={`flex-1`}>
+        {title}
+    </span>
+    </button>
   )
 }
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import Image from "next/image"
 import CustomButton from "./CustomButton"
 
 const Hero = () => {
+  const handleScroll = () => {
+
+  }
   return (
     <div className="hero">
         <div className="flex-1 pt-36 padding-x">
@@ -11,7 +16,16 @@ const Hero = () => {
             <p className="hero__subtitle">
               Streamline your car rental experience with our effortless booking process. 
             </p>
-            <CustomButton/>
+            <CustomButton
+            title="Explore Cars"
+            containerStyles = "bg-primary-blue text-white rounded-full mt-10"
+            handleClick = {handleScroll}
+            />
+            <div className="hero__image-container">
+              <div className="hero__image">
+                <Image src={}
+              </div>
+            </div>
         </div>
     </div>
   )
